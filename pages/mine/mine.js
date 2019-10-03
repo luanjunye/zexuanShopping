a06856f2abda652116daf9efb9bf35c18ef6ce1b
Muse丶 <554953278@qq.com>
2019-10-03 23:50:54 +0800

    个人中心UI完成初版

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -5,7 +5,111 @@ Page({
    * 页面的初始数据
    */
   data: {
-    images: {}
+    images: {},
+    icons: {},
+    other_f: {},
+    other_s: {},
+    status: [{
+        "id": 1,
+        "url": "/pages/mine/assets/payment.png",
+        "webUrl": "",
+        "title": "待付款",
+        "content": 1,
+
+      },
+      {
+        "id": 2,
+        "url": "/pages/mine/assets/deliver.png",
+        "webUrl": "",
+        "title": "待发货",
+        "content": 0,
+
+      }, {
+        "id": 3,
+        "url": "/pages/mine/assets/receiving.png",
+        "webUrl": "",
+        "title": "待收货",
+        "content": 0,
+
+      },
+      {
+        "id": 4,
+        "url": "/pages/mine/assets/evaluate.png",
+        "webUrl": "",
+        "title": "待评价",
+        "content": 0,
+      },
+      {
+        "id": 5,
+        "url": "/pages/mine/assets/sale.png",
+        "webUrl": "",
+        "title": "退款售后",
+        "content": 0,
+
+      }
+    ],
+    others_first: [{
+        "id": 6,
+        "url": "/pages/mine/assets/address.png",
+        "webUrl": "",
+        "title": "收货地址",
+        "content": 1,
+
+      },
+      {
+        "id": 7,
+        "url": "/pages/mine/assets/collection.png",
+        "webUrl": "",
+        "title": "我的收藏",
+        "content": 0,
+
+      }, {
+        "id": 8,
+        "url": "/pages/mine/assets/mail.png",
+        "webUrl": "",
+        "title": "我的私信",
+        "content": 0,
+
+      },
+      {
+        "id": 9,
+        "url": "/pages/mine/assets/cheat.png",
+        "webUrl": "",
+        "title": "反作弊",
+        "content": 0,
+      }
+    ],
+    others_second: [{
+        "id": 10,
+        "url": "/pages/mine/assets/customer.png",
+        "webUrl": "",
+        "title": "官方客服",
+        "content": 1,
+
+      },
+      {
+        "id": 11,
+        "url": "/pages/mine/assets/enterprise.png",
+        "webUrl": "",
+        "title": "公司简介",
+        "content": 0,
+
+      }, {
+        "id": 12,
+        "url": "/pages/mine/assets/group.png",
+        "webUrl": "",
+        "title": "团购优惠",
+        "content": 0,
+
+      },
+      {
+        "id": 13,
+        "url": "/pages/mine/assets/contact.png",
+        "webUrl": "",
+        "title": "联系我们",
+        "content": 0,
+      }
+    ]
   },
 
   imageLoad: function(e) {
@@ -27,6 +131,65 @@ Page({
     })
   },
 
+  iconLoad: function(e) {
+    var $width = e.detail.width, //获取图片真实宽度
+      $height = e.detail.height,
+      //图片的真实宽高比例
+      ratio = $width / $height;
+    var viewWidth = 63, //设置图片显示宽度，左右留有16rpx边距
+      //计算的高度值
+      viewHeight = 63 / ratio;
+    var icon = this.data.icons;
+    //将图片的datadata-index作为image对象的key,然后存储图片的宽高值
+    icon[e.target.dataset.index] = {
+      width: viewWidth,
+      height: viewHeight
+    }
+    console.log(icon)
+    this.setData({
+      icons: icon,
+    })
+  },
+
+  otherfLoad: function(e) {
+    var $width = e.detail.width, //获取图片真实宽度
+      $height = e.detail.height,
+      //图片的真实宽高比例
+      ratio = $width / $height;
+    var viewWidth = 63, //设置图片显示宽度，左右留有16rpx边距
+      //计算的高度值
+      viewHeight = 63 / ratio;
+    var otherf = this.data.other_f;
+    //将图片的datadata-index作为image对象的key,然后存储图片的宽高值
+    otherf[e.target.dataset.index] = {
+      width: viewWidth,
+      height: viewHeight
+    }
+    console.log(otherf)
+    this.setData({
+      other_f: otherf,
+    })
+  },
+
+  othersLoad: function(e) {
+    var $width = e.detail.width, //获取图片真实宽度
+      $height = e.detail.height,
+      //图片的真实宽高比例
+      ratio = $width / $height;
+    var viewWidth = 63, //设置图片显示宽度，左右留有16rpx边距
+      //计算的高度值
+      viewHeight = 63 / ratio;
+    var others = this.data.other_s;
+    //将图片的datadata-index作为image对象的key,然后存储图片的宽高值
+    others[e.target.dataset.index] = {
+      width: viewWidth,
+      height: viewHeight
+    }
+    console.log(others)
+    this.setData({
+      other_s: others,
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
