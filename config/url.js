dd150b8a673aa759534c0451ca99452162121e56
Muse丶 <554953278@qq.com>
2019-10-17 10:43:41 +0800

    主页接口完成

--- config/url.js
+++ config/url.js
@@ -1,6 +1,7 @@
 const API_BASE_URL = 'http://192.168.0.174:8081/freeter-api/';
 module.exports = {
-  IndexUrlBanner: API_BASE_URL + 'bannerlist', //首页banner图
-  IndexUrlQuick: API_BASE_URL + 'categoryindex', //首页快捷频道
+  IndexUrlBanner: API_BASE_URL + 'index/bannerlist', //首页banner图
+  IndexUrlQuick: API_BASE_URL + 'index/categoryindex', //首页快捷频道
+  IndexUrlOrder: API_BASE_URL + 'index/goodsindex',//首页全部商品
   IndexUrlLogin:API_BASE_URL + 'login/login',//登陆
 };
--- pages/index/index.js
+++ pages/index/index.js
@@ -8,124 +8,37 @@ const app = getApp()
 Page({
   // 主页
   data: {
-    // // 头部导航栏的高度
-    // statusBarHeight: app.globalData.statusBarHeight,
+
     banner: [],
     categoryIndex: [],
     details: []
   },
   onLoad: function(options) {
     this.getIndexData();
-    //console.log(this.details)
+  },
+  onshow: function(options) {
+
   },
   getIndexData: function() {
     let that = this;
     var data = new Object();
     util.request(api.IndexUrlBanner, "GET").then(function(res) {
-      if (res.retCode == 0) {
-        data.banner = res.retData.banner
+      if (res.code === 0) {
+        data.banner = res.banner
         that.setData(data);
       }
     });
     util.request(api.IndexUrlQuick, "GET").then(function(res) {
-      if (res.retCode == 0) {
-        data.categoryIndex = res.retData.categoryIndex
-        data.details = [{
-            id: 1,
-            label: "自营",
-            relateId: 0,
-            title: "煲汤材料炖汤清补",
-            description: "漂亮得不像实力派",
-            price: 59.9,
-            number: "365人付款",
-            place: "广州",
-            free: "包邮",
-            type: 0,
-            picUrl: "/pages/index/assets/tang.png",
-            tag: "满减"
-          },
-          {
-            id: 2,
-            label: "自营",
-            relateId: 0,
-            title: "煲汤材料炖汤清补",
-            description: "哑光表面、专业级调音",
-            price: 59.9,
-            number: "365人付款",
-            place: "广州",
-            free: "包邮",
-            type: 0,
-            picUrl: "/pages/index/assets/tang.png"
-          },
-          {
-            id: 3,
-            label: "自营",
-            relateId: 0,
-            title: "煲汤材料炖汤清补",
-            description: "TPU 环保材质、完美贴合",
-            price: 59.9,
-            number: "365人付款",
-            place: "广州",
-            free: "包邮",
-            type: 0,
-            picUrl: "/pages/index/assets/tang.png"
-          },
-          {
-            id: 4,
-            label: "自营",
-            relateId: 0,
-            title: "煲汤材料炖汤清补",
-            description: "经典配色、专业调音、高品质麦克风",
-            price: 59.9,
-            number: "365人付款",
-            place: "广州",
-            free: "包邮",
-            type: 0,
-            picUrl: "/pages/index/assets/tang.png"
-          },
-          {
-            id: 5,
-            label: "自营",
-            relateId: 0,
-            title: "煲汤材料炖汤清补",
-            description: "经典配色、专业调音、高品质麦克风",
-            price: 59.9,
-            number: "365人付款",
-            place: "广州",
-            free: "包邮",
-            type: 0,
-            picUrl: "/pages/index/assets/tang.png"
-          },
-          {
-            id: 6,
-            label: "自营",
-            relateId: 0,
-            title: "煲汤材料炖汤清补",
-            description: "经典配色、专业调音、高品质麦克风",
-            price: 59.9,
-            number: "365人付款",
-            place: "广州",
-            free: "包邮",
-            type: 0,
-            picUrl: "/pages/index/assets/tang.png"
-          },
-          {
-            id: 7,
-            label: "自营",
-            relateId: 0,
-            title: "煲汤材料炖汤清补",
-            description: "经典配色、专业调音、高品质麦克风",
-            price: 59.9,
-            number: "365人付款",
-            place: "广州",
-            free: "包邮",
-            type: 0,
-            picUrl: "/pages/index/assets/tang.png"
-          }
-        ]
+      if (res.code === 0) {
+        data.categoryIndex = res.category
         that.setData(data);
-        console.log(data)
       }
     });
+    util.request(api.IndexUrlOrder, "GET").then(function(res) {
+      if (res.code === 0) {
+        data.details = res.goodsIndexVOList
+        that.setData(data);
+      }
+    })
   }
 })
