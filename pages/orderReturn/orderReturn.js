77e7244f5fb51fa61f8990e2f2c9457f472ea0d5
KyleBing <kylebing@163.com>
2019-10-17 18:14:35 +0800

    添加 退货页面 - 上划窗口 picker

--- pages/orderReturn/orderReturn.js
+++ pages/orderReturn/orderReturn.js
@@ -1,11 +1,13 @@
 // pages/orderReturn/index.js
 Page({
   data: {
-    packageListArray: ['顺丰','韵达','中通','中通'],
+    packageListArray: ['顺丰', '韵达', '中通', '中通'],
+    reasonArray: ['不喜欢/效果差', '质量问题', '材料与商品描述不符', '大小尺寸与商品描述不符', '卖家发错货', '收到商品少件或破损'],
+
     productName: String,
     productMeta: String,
     productImg: String,
-    returnReason: String,
+    returnReason: 0,
     returnPackageId: '73147861342',
     returnPackageCompany: 0,
     returnPrice: Number,
@@ -13,11 +15,27 @@ Page({
     returnEvidencePic: Array
   },
 
-  bindPickerChange: function(e) {
-    console.log( e.detail.value)
+  // 处理 reason picker
+  handleReasonPickerChange: function(e) {
     this.setData({
-      index: e.detail.value
-    })
+      returnReason: e.detail.value
+    });
+  },
+
+
+  // 处理 packageArray picker
+  handlePackagePickerChange: function (e) {
+    this.setData({
+      returnPackageCompany: e.detail.value
+    });
+  },
+
+  // 处理 input 数据双向绑定
+  handleInputChange: function(e) {
+    let targetData = e.currentTarget.dataset.modal;
+    let currentValue = e.detail.value;
+    this[targetData] = currentValue;
+    console.log(this[targetData])
   },
 
   onLoad: function(options) {
