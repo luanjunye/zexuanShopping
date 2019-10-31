95d9f4fc00c43740504d2beaac415fdd10e9d995
KyleBing <kylebing@163.com>
2019-10-31 18:10:20 +0800

    修复：信息页，下拉不恢复的问题

--- config/url.js
+++ config/url.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://192.168.0.176:8081/freeterapi/';
-//const API_BASE_URL = 'https://shop.zexuanshipin.com/freeterapi/';
+// const API_BASE_URL = 'http://192.168.0.176:8081/freeterapi/';
+const API_BASE_URL = 'https://shop.zexuanshipin.com/freeterapi/';
 
 module.exports = {
   //首页
